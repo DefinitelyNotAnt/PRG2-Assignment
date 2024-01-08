58b0daa7ceffd570a39d8342441e29207c2135d8
ThomasNg0402 <s10259398@connect.np.edu.sg>
2024-01-08 11:40:46 +0800

    v1.6

--- IceCreamShop/icecream.cs
+++ IceCreamShop/icecream.cs
@@ -1,8 +1,38 @@
 namespace IceCreamShop{
-    public class IceCream
+    //Class Flavour
+    public class Flavour
     {
-        private string name;
-        private string description;
-        private string premium;
+        private string type;
+        private bool premium;
+        private int quantity;
+        public string Type {
+            get { return type; }
+            set { type = value; }
+        }
+        public bool Premium
+        {
+            get { return premium; }
+            set { premium = value; }
+        }
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        public Flavour() { }
+        public Flavour(string type, bool premium, int quantity)
+        {
+            this.type = type;
+            this.premium = premium;
+            this.quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return "Type: " + Type + " Premium: " + Premium + " Quantity: " + Quantity; 
+        }
+    }
+    public class Topping
+    {
+
     }
 }
